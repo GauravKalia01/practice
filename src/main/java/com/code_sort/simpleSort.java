391d12993148d33b3cf7a7eadf644e6e568a2ee5
Gaurav Kalia <gauravkalia01@cybage.com>
2019-03-04 15:41:27 +0530

    first commit to stage for demo project

---
+++ src/main/java/com/code_sort/simpleSort.java
@@ -0,0 +1,11 @@
+package com.code_sort;
+
+public class simpleSort {
+
+	public String Sample() {
+		return "sample";
+	}
+	public static void main( String[] args ) {
+		System.out.println("test maven");
+	}
+}
---
+++ src/test/java/com/code_sort/sorting.java
@@ -0,0 +1,14 @@
+package com.code_sort;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class sorting {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
