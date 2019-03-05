a933c44d15e604ce1fc17cc276f5c940e384e7ff
Jagdishso <jagdishsonar1@gmail.com>
2019-03-05 14:57:36 +0530

    Commited by Jagdish

--- src/main/java/com/code_sort/simpleSort.java
+++ src/main/java/com/code_sort/simpleSort.java
@@ -8,5 +8,8 @@ public class simpleSort {
 	public static void main( String[] args ) {
 		System.out.println("test maven !");
 		System.out.println("test maven !");
+		System.out.println("test maven123 !");
+
+
 	}
 }
