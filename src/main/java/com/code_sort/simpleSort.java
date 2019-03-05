7337e5d0bcdd01950c0de525d28eefb06bf4649e
GauravKalia01 <46051451+GauravKalia01@users.noreply.github.com>
2019-03-05 14:59:31 +0530

    Update simpleSort.java
    test for jagdish

--- src/main/java/com/code_sort/simpleSort.java
+++ src/main/java/com/code_sort/simpleSort.java
@@ -8,4 +8,5 @@ public class simpleSort {
 	public static void main( String[] args ) {
 		System.out.println("test maven !");
 		System.out.println("test maven !");
-		System.out.println("test maven ! gaurav changes"); }}
+		System.out.println("test maven ! gaurav changes");
+		System.out.println("test maven ! Jagdish is not able to take pull"); }}
