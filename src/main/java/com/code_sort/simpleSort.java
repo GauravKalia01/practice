fa942bfc746d3ea320e7f3d176b28a174cda317e
Gaurav Kalia <gauravkalia01@cybage.com>
2019-03-05 16:28:57 +0530

    final code 2

--- src/main/java/com/code_sort/simpleSort.java
+++ src/main/java/com/code_sort/simpleSort.java
@@ -21,7 +21,8 @@ public class simpleSort {
 		System.out.println("lets conflict !");
 
 		for (int i =0;i<10;i++){
-			System.out.println("learning new tech.!");
+			System.out.println("learning !");
+			System.out.println("learning !");
 		}
 		System.out.println("lets finished first !");
 
