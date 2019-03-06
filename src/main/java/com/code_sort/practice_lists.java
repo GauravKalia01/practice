44e4937f8d78d40568bae91f5f92ed3c96b8775f
Gaurav Kalia <gauravkalia01@cybage.com>
2019-03-06 18:00:07 +0530

    added a line in practiceList class

--- src/main/java/com/code_sort/practice_lists.java
+++ src/main/java/com/code_sort/practice_lists.java
@@ -43,6 +43,8 @@ public class practice_lists {
     List copiedList = new ArrayList();
         copiedList.addAll(StackList);
         System.out.print("Elements in copied list" + copiedList);
+
+      System.out.print("testing for branch");
     }
 
 }
